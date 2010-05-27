2ba7c07e3ab204b3a2e6ed8fd47c2172a964962b
Andy <andy@onthewings.net>
2010-05-28 05:52:57 +0800

    working on a sample

--- GraphicalTest.hx
+++ GraphicalTest.hx
@@ -31,26 +31,37 @@ using Lambda;
 class GraphicalTest extends Sprite{
 	public var schemes:Array<Dynamic>;
 	public var arcticView:ArcticView;
+	public var primaryColorBlock:MutableBlock;
 
 	public function new():Void {
 		super();
 
 		schemes = [Analogous,Complementary,Compound,FlippedCompound,Monochrome,SplitComplementary,Tetrad,Triad];
-		
-		var schemeS = Arctic.makeTextChoiceBlocks(schemes.map(getClassName).array(),switchToScheme).blocks;
-		schemeS.unshift(Arctic.makeText("Color schemes"));
-		
+		primaryColorBlock = new MutableBlock(Background(0x0000FF, Fixed(20,20)));
 		var gui = 
 			Border( 5, 5,
 				Frame( 2, 0x000000, 
-					Background(0xDDDDDD, 
+					Background(0xFFFFFF, 
 						Border( 10, 10,
-							ColumnStack( [ 
-								LineStack(schemeS)
+							LineStack( [ 
+								Arctic.makeText("Primary color"),
+								ColumnStack([
+									Mutable(primaryColorBlock),
+									TextInput("0000FF",50,20,changePrimaryColor,null,6,null,0xFFFFFF,true)
+								]),
+								Fixed(null,15),
+								Arctic.makeText("Color schemes"),
+								LineStack(
+									Arctic.makeTextChoiceBlocks(schemes.map(getClassName).array(),switchToScheme).blocks
+								),
+								Fixed(null,15),
+								Frame( 2, 0x000000,
+									Arctic.makeSimpleButton("Run",run),
+								10, 50, 0, 0)
 							] )
 						), 
-					null, 10),
-				10, null, 0, 0)
+					80, 10),
+				10, 50, 0, 0)
 			);
 
 		arcticView = new ArcticView( Arctic.makeDragable(true,true,true,gui).block, ArcticMC.getCurrentClip() );
@@ -64,7 +75,22 @@ class GraphicalTest extends Sprite{
 		return (dotPos > -1) ? s.substr(dotPos+1) : s;
 	}
 
+	private function changePrimaryColor(s:String):Bool {
+		s = s.toUpperCase();
+		for (char in s.split("")) {
+			if ("1234567890ABCDEF".indexOf(char) == -1) return false;
+		}
+		
+		var color = Std.parseInt("0x"+s);
+		primaryColorBlock.block = Background(color, Fixed(20,20));
+		return true;
+	}
+
 	private function switchToScheme(i:Int,s:String):Void {
+	
+	}
+
+	private function run():Void {
 	
 	}
 
